082184f07e1a08307a6f96d6c6282858756bbad5
Dean Chen <dean@ocirs.com>
2011-03-20 14:09:05 -0400

    Finished .jade template

--- image_rotator/app/app.js
+++ image_rotator/app/app.js
@@ -3,7 +3,7 @@
  */
 
 var express = require('express');
-
+var stylus = require('stylus');
 // Path to our public directory
 
 var pub = __dirname + '/public';
@@ -20,6 +20,23 @@ app.set('view options', {
   layout: false
 });
 
+function compile(str, path, fn) {
+  stylus(str)
+    .set('filename', path)
+    .set('compress', true)
+    .render(fn);
+};
+
+// add the stylus middleware, which re-compiles when
+// a stylesheet has changed, compiling FROM src,
+// TO dest. dest is optional, defaulting to src
+
+app.use(stylus.middleware({
+    src: __dirname + '/views'
+  , dest: __dirname + '/public'
+  , compile: compile
+}));
+
 // Optional since express defaults to CWD/views
 
 app.set('views', __dirname + '/views');
@@ -29,9 +46,55 @@ app.set('views', __dirname + '/views');
 // (although you can still mix and match)
 app.set('view engine', 'jade');
 
+function imageElement(headline, date, caption, imageUrl) {
+  this.headline = headline;
+  this.date = date;
+  this.caption = caption;
+  this.imageUrl = imageUrl;
+}
+
+// Dummy users
+var imageElements = [
+    new imageElement(
+    	'Slowing Down', 
+    	'04/10/09', 
+    	'Autem conventio nimis quis ad, nisl secundum sed, facilisi, vicis augue regula, ratis, autem. Neo nostrud letatio aliquam validus eum quadrum, volutpat et.<br /><a href="http://store.glennz.com/slowingdown.html" target="_blank">Artwork By Glenn Jones</a>',
+    	'images/banner1.jpg'
+    ),
+  	new imageElement(
+    	'Organized Food Fight', 
+    	'04/11/09', 
+    	'Autem conventio nimis quis ad, nisl secundum sed, facilisi, vicis augue regula, ratis, autem. Neo nostrud letatio aliquam validus eum quadrum, volutpat et.<br /><a href="http://store.glennz.com/slowingdown.html" target="_blank">Artwork By Glenn Jones</a>',
+    	'images/banner2.jpg'
+    ),
+    new imageElement(
+    	'Endangered Species', 
+    	'04/12/09', 
+    	'Autem conventio nimis quis ad, nisl secundum sed, facilisi, vicis augue regula, ratis, autem. Neo nostrud letatio aliquam validus eum quadrum, volutpat et.<br /><a href="http://store.glennz.com/slowingdown.html" target="_blank">Artwork By Glenn Jones</a>',
+    	'images/banner3.jpg'
+    ),  	
+    new imageElement(
+    	'Evolution', 
+    	'04/13/09', 
+    	'Autem conventio nimis quis ad, nisl secundum sed, facilisi, vicis augue regula, ratis, autem. Neo nostrud letatio aliquam validus eum quadrum, volutpat et.<br /><a href="http://store.glennz.com/slowingdown.html" target="_blank">Artwork By Glenn Jones</a>',
+    	'images/banner4.jpg'
+    ),  	
+    new imageElement(
+    	'Puzzled Putter', 
+    	'04/14/09', 
+    	'Autem conventio nimis quis ad, nisl secundum sed, facilisi, vicis augue regula, ratis, autem. Neo nostrud letatio aliquam validus eum quadrum, volutpat et.<br /><a href="http://store.glennz.com/slowingdown.html" target="_blank">Artwork By Glenn Jones</a>',
+    	'images/banner5.jpg'
+    ),  	
+    new imageElement(
+    	'Secret Habit', 
+    	'04/15/09', 
+    	'Autem conventio nimis quis ad, nisl secundum sed, facilisi, vicis augue regula, ratis, autem. Neo nostrud letatio aliquam validus eum quadrum, volutpat et.<br /><a href="http://store.glennz.com/slowingdown.html" target="_blank">Artwork By Glenn Jones</a>',
+    	'images/banner6.jpg'
+    ),
+];
 
 app.get('/', function(req, res){
-  res.render('rotator');
+  res.render('rotator', {elements: imageElements});
 });
 
 app.listen(3000);
